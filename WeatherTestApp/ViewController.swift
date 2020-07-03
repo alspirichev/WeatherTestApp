956d4043722510122fa8bb7543a9199284e934fd
Alexander Spirichev <alspirichev@gmail.com>
2020-07-03 16:16:21 +0300

    Initial Commit

---
+++ WeatherTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherTestApp
+//
+//  Created by Alexander Spirichev on 03.07.2020.
+//  Copyright © 2020 Alexander Spirichev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
