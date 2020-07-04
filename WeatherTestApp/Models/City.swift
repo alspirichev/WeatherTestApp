64f31f1a521c163a6bca8d757a2664832e9004e4
Alexander Spirichev <alspirichev@gmail.com>
2020-07-04 22:21:47 +0300

    Add Hashable for City model

--- WeatherTestApp/Models/City.swift
+++ WeatherTestApp/Models/City.swift
@@ -8,8 +8,18 @@
 
 import Foundation
 
-struct City: Decodable, Hashable {
+struct City: Decodable {
     let id: Int
     let name: String
     let country: String
+}
+
+extension City: Hashable {
+    static func == (lhs: City, rhs: City) -> Bool {
+        return lhs.name == rhs.name
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+    }
 }
