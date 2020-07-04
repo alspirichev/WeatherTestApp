e83e6552174ae7c3fa7b8686d1caaeebd1f290d9
Alexander Spirichev <alspirichev@gmail.com>
2020-07-04 15:23:28 +0300

    Setup UI

--- WeatherTestApp/AppDelegate.swift
+++ WeatherTestApp/AppDelegate.swift
@@ -11,8 +11,19 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let storyboard = UIStoryboard(name: "WeatherViewController", bundle: nil)
+        let initialViewController = storyboard.instantiateViewController(withIdentifier: "WeatherViewController")
+        window.rootViewController = initialViewController
+        
+        self.window = window
+        window.makeKeyAndVisible()
+        
         return true
     }
 
--- WeatherTestApp/WeatherViewController.swift
+++ WeatherTestApp/WeatherViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeatherViewController.swift
 //  WeatherTestApp
 //
 //  Created by Alexander Spirichev on 03.07.2020.
@@ -8,13 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherViewController: UIViewController, ViewSpecificController {
+    typealias RootView = WeatherView
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
     }
 
-
 }
-
